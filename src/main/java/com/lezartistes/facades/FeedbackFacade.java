cfc6ea481b52b7dfa85d21bfa3ae4edfeb8cc8e2
Ophélie Amarine <ophelie.amarine@etu.umontpellier.fr>
2021-12-13 11:03:07 +0100

    models for Company, Expert, Feedback and FeedbackFacade

---
+++ src/main/java/com/lezartistes/facades/FeedbackFacade.java
@@ -0,0 +1,14 @@
+package com.lezartistes.facades;
+
+import com.lezartistes.models.Feedback;
+
+public class FeedbackFacade {
+
+    public Feedback getFeedbackByExpert(String conpanyName){
+        return null;
+    }
+
+    public Feedback getAllFeedbackByExpert(String conpanyName){
+        return null;
+    }
+}
