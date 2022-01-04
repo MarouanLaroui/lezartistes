19ede6052b4245518459b2acc013a7c03aab9733
Ophélie Amarine <ophelie.amarine@etu.umontpellier.fr>
2022-01-04 11:42:44 +0100

    Feedback controller and feedbackFacade

--- src/main/java/com/lezartistes/controllers/feedback/FeedbackController.java
+++ src/main/java/com/lezartistes/controllers/feedback/FeedbackController.java
@@ -1,4 +1,16 @@
 package com.lezartistes.controllers.feedback;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class FeedbackController {
+
+    /**
+     * Linked to the TextField who's fx:id is his rating
+     */
+    @FXML
+    private Label rating;
+
+    @FXML
+    private Label comment;
 }
--- src/main/java/com/lezartistes/facades/FeedbackFacade.java
+++ src/main/java/com/lezartistes/facades/FeedbackFacade.java
@@ -6,11 +6,6 @@ import java.util.List;
 
 public class FeedbackFacade {
 
-    public Feedback getFeedbackByCompany(String companyName){
-
-        return null;
-    }
-
     public List<Feedback> getAllFeedbackByCompany(String companyName){
 
         return null;
