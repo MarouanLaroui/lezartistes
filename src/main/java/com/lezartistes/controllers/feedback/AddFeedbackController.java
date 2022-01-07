4c030ecc3c63373348aa8e80671d54c6272e5581
Ophélie Amarine <ophelie.amarine@etu.umontpellier.fr>
2022-01-07 15:08:07 +0100

    addFeedback navbar

--- src/main/java/com/lezartistes/controllers/feedback/AddFeedbackController.java
+++ src/main/java/com/lezartistes/controllers/feedback/AddFeedbackController.java
@@ -1,24 +1,20 @@
 package com.lezartistes.controllers.feedback;
 
-import com.lezartistes.App;
-import com.lezartistes.dao.feedback.FeedbackDAO;
+import com.lezartistes.controllers.GeneralController;
 import com.lezartistes.exceptions.CompanyNotFoundException;
 import com.lezartistes.facades.CompanyFacade;
 import com.lezartistes.facades.FeedbackFacade;
 import com.lezartistes.models.Company;
 import com.lezartistes.models.Feedback;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class AddFeedbackController{
+public class AddFeedbackController extends GeneralController {
 
     @FXML
     private TextArea comment;
