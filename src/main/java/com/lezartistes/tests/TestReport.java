d63684d4c16bdadc925e725117568f15119fe689
julien-vendran <julien.vendran@etu.umontpellier.fr>
2022-01-11 00:15:32 +0100

    Tous les tests passent

--- src/main/java/com/lezartistes/tests/TestReport.java
+++ src/main/java/com/lezartistes/tests/TestReport.java
@@ -20,8 +20,6 @@ public class TestReport {
         Report r = new Report("t", "Test",false, new Date(2020, Calendar.AUGUST,2),"team","n","n",12,"2","obs",null,null,null);
 
         Assertions.assertNotEquals(null, reportFacade.createReport(r));
-        reportFacade.deleteReport(r.getId());
-
     }
 
 }
